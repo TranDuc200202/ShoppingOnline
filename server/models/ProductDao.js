b322df05bd1652179e886f15f7ddec751c4b31d9
ACER <tranduc200202@gmail.com>
2023-06-27 16:13:17 +0700

    first commit

---
+++ server/models/ProductDao.js
@@ -0,0 +1,69 @@
+require('../utils/MongooseUtil');
+const Models = require('./Models');
+
+const ProductDAO = {
+  //Admin
+  async selectAll() {
+    const query = {};
+    const products = await Models.Product.find(query).exec();
+    return products;
+  },
+  async insert(product) {
+    const mongoose = require('mongoose');
+    product._id = new mongoose.Types.ObjectId();
+    const result = await Models.Product.create(product);
+    return result;
+  },
+  //Update
+  async selectByID(_id) {
+    const product = await Models.Product.findById(_id).exec();
+    return product;
+  },
+  async update(product) {
+    const newvalues = { name: product.name, price: product.price, image: product.image, category: product.category };
+    const result = await Models.Product.findByIdAndUpdate(product._id, newvalues, { new: true });
+    return result;
+  },
+  //Delete
+  async delete(_id) {
+    const result = await Models.Product.findByIdAndRemove(_id);
+    return result;
+  },
+  //Customer
+  async selectByID(_id) {
+    const product = await Models.Product.findById(_id).exec();
+    return product;
+  },
+  async selectTopNew(top) {
+    const query = {};
+    const mysort = { cdate: -1 }; // descending
+    const products = await Models.Product.find(query).sort(mysort).limit(top).exec();
+    return products;
+  },
+  async selectTopHot(top) {
+    const items = await Models.Order.aggregate([
+      { $match: { status: 'APPROVED' } },
+      { $unwind: '$items' },
+      { $group: { _id: '$items.product._id', sum: { $sum: '$items.quantity' } } },
+      { $sort: { sum: -1 } }, // descending
+      { $limit: top }
+    ]).exec();
+    var products = [];
+    for (const item of items) {
+      const product = await ProductDAO.selectByID(item._id);
+      products.push(product);
+    }
+    return products;
+  },
+  async selectByCatID(_cid) {
+    const query = { 'category._id': _cid };
+    const products = await Models.Product.find(query).exec();
+    return products;
+  },
+  async selectByKeyword(keyword) {
+    const query = { name: { $regex: new RegExp(keyword, "i") } };
+    const products = await Models.Product.find(query).exec();
+    return products;
+  }
+};
+module.exports = ProductDAO;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'tranduc.nfwd9ti.mongodb.net',
+    DB_USER: 'tranduc200202',
+    DB_PASS: '0984215460',
+    DB_DATABASE: 'Shoppingonline',
+    EMAIL_USER: 'tranduc200202@gmail.com', // gmail cá nhân
+    EMAIL_PASS: 'hjmcnzsafnyyfhtu', //password : hjmcnzsafnyyfhtu
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '1800000', // in milliseconds
+  };
+  module.exports = MyConstants; 
